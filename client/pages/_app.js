32567f4563946119d75d63ea44be83ad88b5a64a
Hugh Ferguson <hugh@hferguson.ca>
2023-02-21 19:37:48 -0500

    Section 22 505

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,9 @@ const AppComponent =  ( {Component, pageProps, currentUser} ) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className="container">
+      <Component currentUser={currentUser}  {...pageProps} />
+      </div>
     </div>
   )
 };
@@ -19,7 +21,7 @@ AppComponent.getInitialProps = async (appContext) => {
     const {data} = await client.get('/api/users/currentuser');
     let pageProps = {};
     if (appContext.Component.getInitialProps) {
-        pageProps = await appContext.Component.getInitialProps(context);
+        pageProps = await appContext.Component.getInitialProps(context, client, data.currentUser);
     }
     
 
