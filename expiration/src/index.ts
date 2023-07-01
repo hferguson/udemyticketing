0cc0c81a3c97b67f148ef0f4fafdeb94e8ec4a27
Hugh Ferguson <hugh@hferguson.ca>
2023-07-01 14:22:53 -0400

    Trigger rebuild

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -4,7 +4,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 
 const start = async () => {
-  
+  console.log("Expiration service starting up");
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
