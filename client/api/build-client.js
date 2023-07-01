c2407e2f378d151a462381c9feca95788e096d13
Hugh Ferguson <hugh@hferguson.ca>
2023-07-01 15:13:16 -0400

    Updated PROD config with domain name

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     // we are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://hfcdevops.xyz/',
       headers: req.headers,
     });
   }else {
